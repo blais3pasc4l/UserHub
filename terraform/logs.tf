c01524800325e1443caa2fdf71f39b4c4fb750fc
Juan David Calderon <juandavidcalderonpena@gmail.com>
2023-07-05 11:28:44 -0500

    Initial commit: Setting up project infrastructure and dependencies

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -1,9 +1,9 @@
-resource "aws_cloudwatch_log_group" "django-log-group" {
-  name              = "/ecs/django-app"
+resource "aws_cloudwatch_log_group" "log-group" {
+  name              = "/ecs/app"
   retention_in_days = var.log_retention_in_days
 }
 
-resource "aws_cloudwatch_log_stream" "django-log-stream" {
-  name           = "django-app-log-stream"
-  log_group_name = aws_cloudwatch_log_group.django-log-group.name
+resource "aws_cloudwatch_log_stream" "log-stream" {
+  name           = "app-log-stream"
+  log_group_name = aws_cloudwatch_log_group.log-group.name
 }
